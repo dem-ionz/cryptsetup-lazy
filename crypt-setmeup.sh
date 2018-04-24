6ceabca111ab62e53489a9e242d44c80c93eaafd
Nullsoulness ~# <31750444+dem-ionz@users.noreply.github.com>
2018-04-24 07:30:31 -0400

    Create crypt-setmeup.sh

---
+++ crypt-setmeup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Is CMake even needed? Lol.
+
+if ! [ $(id -u) = 0 ]; then
+   echo "How the fuck am I supposed to do this shit without root accesss? Run me as root!"
+   exit 1
+fi
+
+apt-get update
+apt-get install uuid-dev autopoint build-essential libpopt-dev libdevmapper-dev git cmake libjson-c-dev
+git clone https://gitlab.com/cryptsetup/cryptsetup.git
+cd cryptsetup
+sh autogen.sh
+./configure
+make
+make install
+echo "Should be Installed now!"
+echo "Running cryptsetup command to check."
+echo "If you see output you're good. :)"
