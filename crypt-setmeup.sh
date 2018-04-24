2bac07931ded7ae06702969489040d66b8aed36c
Nullsoulness ~# <31750444+dem-ionz@users.noreply.github.com>
2018-04-24 07:42:18 -0400

    Update crypt-setmeup.sh

--- crypt-setmeup.sh
+++ crypt-setmeup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Is CMake even needed? Lol.
+# Did I include shit twice?
 
 if ! [ $(id -u) = 0 ]; then
    echo "How the fuck am I supposed to do this shit without root accesss? Run me as root!"
@@ -7,7 +8,7 @@ if ! [ $(id -u) = 0 ]; then
 fi
 
 apt-get update
-apt-get install uuid-dev autopoint build-essential libpopt-dev libdevmapper-dev git cmake libjson-c-dev
+apt-get install uuid-dev autopoint build-essential libpopt-dev libdevmapper-dev git cmake libjson-c-dev autoconf
 git clone https://gitlab.com/cryptsetup/cryptsetup.git
 cd cryptsetup
 sh autogen.sh
@@ -15,7 +16,4 @@ sh autogen.sh
 make
 make install
 echo "Should be Installed now!"
-echo "Running cryptsetup command to check."
-echo "If you see output you're good. :)"
-cryptsetup
 echo "https://www.nullsoul.pw/"
